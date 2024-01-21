b32ba5e92adce4eeb64d3c71cef2daed254b8cf7
sagarsingh-0 <sagarsingh95560@gmail.com>
2024-01-21 11:52:55 +0530

    First Check of Logo

---
+++ qtify/src/components/Logo/Logo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import LogoImage from "../../assets/Q tify.png";
+
+const Logo = () => {
+	return (
+			<img src={LogoImage} alt="Logo" width={67} height={34} />
+	);
+};
+
+export default Logo;
