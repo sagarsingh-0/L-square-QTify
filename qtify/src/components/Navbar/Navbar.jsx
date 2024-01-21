6a5a482d2359bede8aa0bbe1821a81bace583042
sagarsingh-0 <sagarsingh95560@gmail.com>
2024-01-21 13:05:35 +0530

    C3: Button

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./Navbar.module.css";
 import Logo from "../Logo/Logo";
 import Search from "../Search/Search";
+import Button from "../Button/Button";
 
 function Navbar(){
 	return (
@@ -10,6 +11,8 @@ function Navbar(){
 					<Logo/>
 				</a>
 				<Search placeholder="Search a album of your choice" />
+				<Button children="Give Feedback"/>
+
 			</nav>
 	)
 }
