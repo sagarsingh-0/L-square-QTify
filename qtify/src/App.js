5008e75497a37470dded56b1b848af615cabf4d7
sagarsingh-0 <sagarsingh95560@gmail.com>
2024-01-22 21:34:51 +0530

    Hero Section

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,9 +1,10 @@
 import Navbar from "./components/Navbar/Navbar";
-
+import Hero from './components/Hero/Hero.jsx';
 function App() {
   return (
     <>
     <Navbar/>
+    <Hero/>
     </>
   );
 }
